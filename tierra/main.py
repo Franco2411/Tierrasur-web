29ca1ba0bfde7bd1feaf64a94bc7f70df9167fd9
Franc2411 <fra98nba@gmail.com>
2021-01-26 08:55:30 -0300

    SDK Firebase Ready

--- tierra/main.py
+++ tierra/main.py
@@ -1,6 +1,9 @@
 from flask import (
     Blueprint, flash, redirect, render_template, request, url_for
 )
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 bp = Blueprint('main', __name__)
 
