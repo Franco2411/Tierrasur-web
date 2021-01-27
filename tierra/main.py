aa8bd47e057e035a56ad70cb45c9199c129ff120
Franc2411 <fra98nba@gmail.com>
2021-01-26 21:49:19 -0300

    Frontend ready, falta backend

--- tierra/main.py
+++ tierra/main.py
@@ -7,6 +7,23 @@ from firebase_admin import firestore
 
 bp = Blueprint('main', __name__)
 
+x = 0
+
 @bp.route('/')
 def index():
+    #initializeFire() 
+    #default_app = firebase_admin.initialize_app()
+    
+
+    #db = firestore.client()
+
+    # Consulto la base
+    #query = db.collection(u'07-01-2021')
+
+    #flash(query)
+    
     return render_template('main.html')
+
+def initializeFire():
+    cred = credentials.Certificate('tierra/tierrasur-sa-8bdf08a269f8.json')
+    firebase_admin.initialize_app(cred)
